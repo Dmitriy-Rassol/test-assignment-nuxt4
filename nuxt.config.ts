cfe2f53806b4fcb44639bc7957612abc88137de4
Dmitriy Rassolenko <rassoldima@yandex.ru>
2026-02-17 10:17:20 +0300

    Подправил config и readme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,40 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: true,
+app: {
+    head: {
+      title: 'Онлайн магазин',
+      htmlAttrs: {
+        lang: 'ru',
+      },
+      link: [{
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/icon.svg',
+      }],
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1',
+        },
+        {
+          name: 'description',
+          content: 'Онлайн магазин обуви',
+        },
+        {
+          name: 'keywords',
+          content: 'Обувь, Магазин обуви',
+        },
+        {
+          charset: 'utf-8',
+        },
+        {
+          name: 'color-scheme',
+          content: 'only light',
+        }
+      ],
+    },
+  },
   css: [
     '~/assets/styles/main.scss'
   ],
